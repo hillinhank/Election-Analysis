ac45e2a86b695d437ef7df486725edfe4b78cb17
hillinhank <101481759+hillinhank@users.noreply.github.com>
2022-04-09 17:54:08 -0500

    stuck in the same spot. it is not returning a row value

--- Pypollnew.py
+++ Pypollnew.py
@@ -2,15 +2,27 @@ import csv
 import os
 
 file_to_load = os.path.join('/Users/user/election_results1.csv')
+file_to_save = os.path.join('/Users/user/try_again1.txt')
 
-election_data = open(file_to_load, 'r')
+# election_data = open(file_to_load, 'r')
 
-# Open the election results and read the file.
+
+total_votes = 0
+
+# Open the election results and read the file
 with open(file_to_load) as election_data:
     file_reader = csv.reader(election_data)
-    # Print the file object.
+
+    # Read the header row.
     headers = next(file_reader)
-    print(headers)
+
+    # Print each row in the CSV file.
+    for row in file_reader:
+        # 2. Add to the total vote count.
+        total_votes += 1
+
+# 3. Print the total votes.
+print(total_votes)
 
 
 
@@ -23,15 +35,15 @@ with open(file_to_load) as election_data:
 
 
 
-file_to_save = os.path.join('/Users/user/try_again1.txt')
-outfile = open(file_to_save, "w")
-outfile.write('helloooo')
-outfile.close()
+#file_to_save = os.path.join('/Users/user/try_again1.txt')
+#outfile = open(file_to_save, "w")
+#outfile.write('helloooo')
+#outfile.close()
 
-file_to_save = os.path.join('/Users/user/try_again1.txt')
-outfile = open(file_to_save, "w")
-outfile.write("Counties in the Election\n--------------------\nArapahoe\nDenver\nJefferson")
-outfile.close()
+#file_to_save = os.path.join('/Users/user/try_again1.txt')
+#outfile = open(file_to_save, "w")
+#outfile.write("Counties in the Election\n--------------------\nArapahoe\nDenver\nJefferson")
+#outfile.close()
 
 
 
