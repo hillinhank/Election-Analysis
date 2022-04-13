97b05d437a3648c5eff42f272343350864dbbc4e
hillinhank <101481759+hillinhank@users.noreply.github.com>
2022-04-12 20:44:26 -0500

    no mistakes yet!!

---
+++ Pypoll_Challenge.py
@@ -0,0 +1,46 @@
+
+from fileinput import close
+import os
+import csv
+# Create a filename variable to a direct or indirect path to the file.
+file_to_save = os.path.join("/Users/user/election_analysis.txt")
+file_to_load = os.path.join("/Users/user/election_results-2.csv")
+# Using the open() function with the "w" mode we will write data to the file.
+txt_file = open(file_to_save, "w")
+election_data = open(file_to_load,"r")
+# Using the with statement open the file as a text file.
+with open(file_to_save, "w") as txt_file:
+
+    # Write some data to the file.
+    txt_file.write("Counties in the Election\n-----------\nArapahoe\nDenver\nJefferson")
+
+txt_file.close()
+
+with open(file_to_load, "r") as election_data:
+    file_reader = csv.reader(election_data)
+    headers = next(file_reader)
+    print(headers)
+    for row in file_reader:
+        print(row)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
